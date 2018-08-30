cb0f3766094b2383594900a08ea733a14f4ef77a
pevnak <pevnak@gmail.com>
2018-08-30 07:09:00 +0200

    forget to add kernels

---
+++ src/kernels.jl
@@ -0,0 +1,42 @@
+abstract type AbstractKernel end;
+
+"""
+	GaussianKernel(γ<:Number)
+
+	implements the standard Gaussian kernel ``exp(-γ * x)
+"""
+struct GaussianKernel{T<:Number} <: AbstractKernel
+	γ::T
+end
+
+(m::GaussianKernel)(x::Number) = exp(-m.γ * x)
+
+
+"""
+	ImqKernel(c)
+
+	inverse polynomial kernel ``\\frac{C}{C + x}`` used
+	in Tolstikhin, Ilya, et al. "Wasserstein Auto-Encoders." arXiv preprint arXiv:1711.01558 (2017)
+
+	`c` being equal to 2d σ2z, which is expected squared distance between two samples drawn from p(x).
+
+"""
+struct IMQKernel{T<:Number} <: AbstractKernel
+	c::T
+end
+
+(m::IMQKernel)(x::Number) = m.c/ (m.c + x)
+
+"""
+	RQ kernel ``\\frac{C}{C + \\|X - Y\\|}`` used
+	in Tolstikhin, Ilya, et al. "Wasserstein Auto-Encoders." arXiv preprint arXiv:1711.01558 (2017)
+
+	`c` being equal to 2d σ2z, which is expected squared distance between two samples drawn from p(x).
+
+"""
+struct RQKernel{T<:Number} <: AbstractKernel
+	α::T
+end
+
+(m::RQKernel)(x::Number) = (1 + (x + eps(x))/2m.α)^-m.α 
+
