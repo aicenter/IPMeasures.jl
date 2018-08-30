401fc9832355156a9e4405d0f40be82c8fca6753
pevnak <pevnak@gmail.com>
2018-08-30 07:08:33 +0200

    reworked API of IPMeasures

--- src/IPMeasures.jl
+++ src/IPMeasures.jl
@@ -1,6 +1,6 @@
 module IPMeasures
 using StatsBase
-
+include("kernels.jl")
 
 samplecolumns(x,n) =  (size(x,2) > n) ? x[:,sample(1:size(x,2),n,replace = false)] : x
 
@@ -17,89 +17,49 @@ pairwisel2(x) = -2 .* x' * x .+ sum(x.^2, dims = 1)' .+ sum(x.^2,dims = 1)
 """
 		k_gaussian(x,y,γ)
 		k_gaussian(x,γ)
+		k_gaussian(d::M, γ, idx, cidx)
 
 		kernel matrix corresponding to the gaussian kernel with `γ` on diagonal
 """
-k_gaussian(x,y,γ) = sum(exp.(-γ .* pairwisel2(x,y)))/(size(x,2) * size(y,2))
-k_gaussian(x::T,γ) where {T<:AbstractMatrix} = sum(exp.(-γ .* pairwisel2(x)))/(size(x,2) * (size(x,2) -1 )) 
-k_gaussian(x::T,γ) where {T<:AbstractVector} = zero(eltype(x)) 
-
-# mapsum(f, d, idx) = sum(f(d[j,i]) for i in idx for j in idx)
-# mapsum(f, d, rowidx, colidx) = sum(f(d[i,j]) for j in colidx for i in rowidx )
-mapsum(f, d, idx) = (s = 0.0; @inbounds for j in idx, i in idx s+= f(d[i,j]) end; s)
-mapsum(f, d, rowidx, colidx) = (s = 0.0; @inbounds for i in rowidx, j in colidx s+= f(d[i,j]) end; s)
-
-null_distribution(x, γ::Real, n) = _null_distribution(pairwisel2(x), γ, n)
-null_distribution(x, γs::Vector, n) = (d = pairwisel2(x); [_null_distribution(d, γ, n) for γ in γs]) 
-function _null_distribution(d, γ, n)
-	d *= -γ
-	l = size(d,2)
-	mapreduce(+, 1:n) do _
-		idx = sample(1:l, div(l,2))
-		cidx = setdiff(1:l, idx)
-		li, lc = length(idx), length(cidx)
-		(mapsum(exp, d, idx) - li)/(li * (li+1)) + (mapsum(exp, d, cidx) - lc)/(lc * (lc+1)) - 2* mapsum(exp, d, idx, cidx)/ (lc*li)
-	end
-end
+kernelsum(k::AbstractKernel,x,y) = sum(k.(pairwisel2(x,y)))/(size(x,2) * size(y,2))
+kernelsum(k::AbstractKernel,x::T) where {T<:AbstractMatrix} = (l = size(x,2); sum(k.(pairwisel2(x)))/(l^2 - l)) 
+kernelsum(k::AbstractKernel,x::T) where {T<:AbstractVector} = zero(eltype(x)) 
 
 
 """
-		mmdg(x,y,γ)
-		mmdg(x,y,γ,n)
+		mmd(AbstractKernel(γ), x, y)
+		mmd(AbstractKernel(γ), x, y, n)
 
 		mmd with gaussian kernel of bandwidth `γ` using at most `n` samples
 """
-mmdg(x,y,γ) = k_gaussian(x,γ) + k_gaussian(y,γ) - 2*k_gaussian(x,y,γ)
-mmdg(x,y,γ,n) = mmdg(samplecolumns(x,n), samplecolumns(y,n), γ)
+mmd(k::AbstractKernel, x, y) = kernelsum(k, x) + kernelsum(k, y) - 2*kernelsum(k, x, y)
+mmd(k::AbstractKernel, x, y, n::Int) = mmd(k, samplecolumns(x,n), samplecolumns(y,n))
+function mmd(k::AbstractKernel, d, idx, cidx)
+	li, lc = length(idx), length(cidx)
+	kxx = mapsum(k, d, idx)/(li^2 - li)
+	kyy = mapsum(k, d, cidx)/(lc^2 - lc)
+	kyx = mapsum(k, d, idx, cidx)/ (lc*li)
+	kxx + kyy -2kyx
+end
 
 
-"""
-		k_imq(x,c,γ)
-		k_imq(x,c)
-
-		kernel matrix corresponding to the inverse multi-quadratic kernel ``imq = \\frac{C}{C + \\|X - Y\\|}`` kernel with `c` on diagonal
-"""
-k_imq(x,y,c) = sum( c./ (c .+ pairwisel2(x,y)))/(size(x,2) * size(y,2))
-k_imq(x::T,c) where {T<:AbstractMatrix} = sum(c ./(c .+ pairwisel2(x)))/(size(x,2) * (size(x,2) -1 )) 
-k_imq(x::T,c) where {T<:AbstractVector} = zero(eltype(x)) 
-
-"""
-		mmd_imq(x,y,γ)
-
-		mmd with inverse polynomial kernel ``\\frac{C}{C + \\|X - Y\\|}`` used
-		in Tolstikhin, Ilya, et al. "Wasserstein Auto-Encoders." arXiv preprint arXiv:1711.01558 (2017)
-
-		`c` being equal to 2d σ2z, which is expected squared distance between two samples drawn from p(x).
+mapsum(f, d, idx) = (s = 0.0; @inbounds for j in idx, i in idx s+= f(d[i,j]) end; s)
+mapsum(f, d, rowidx, colidx) = (s = 0.0; @inbounds for i in rowidx, j in colidx s+= f(d[i,j]) end; s)
 
 """
-mmd_imq(x,y,c) = k_imq(x,c) + k_imq(y,c) - 2*k_imq(x,y,c)
-mmd_imq(x,y,c,n) = mmd_imq(samplecolumns(x,n), samplecolumns(y,n), c)
+		null_distribution(k::AbstractKernel, x, n, l = div(size(x,2),2)) = _null_distribution(k, pairwisel2(x), n, l )
 
+		estimates the null distribution of samples `x` from `n` random draws of subsets of size `l`
 """
+null_distribution(k::AbstractKernel, x, n, l = div(size(x,2),2)) = _null_distribution(k, pairwisel2(x), n, l )
+function _null_distribution(k, d, n, l)
+	ns = size(d,2)
+	map(1:n) do _
+		idx = sample(1:ns, l, replace = false)
+		cidx = setdiff(1:ns, idx)
+		cidx = sample(cidx, min(l, length(cidx)), replace = false) 
+		mmd(k, d, idx, cidx)
+	end
+end
 
-		rqfun(x)
-
-		kernel from 
-"""
-rqfun(x,α) = (1 + (x + eltype(x)(1e-6))/2α)^-α 
-
-"""
-		k_rqh(x,y,α)
-		k_rqh(x,α)
-
-		kernel matrix with rq kernel
-"""
-k_rqh(x,y,α) = sum( 1 .+ rqfun.(pairwisel2(x,y),α))/((size(x,2) - 1) * size(y,2))
-k_rqh(x::T,α) where {T<:AbstractMatrix} = sum( 1 .+ rqfun.(pairwisel2(x),α)) /(size(x,2) * (size(x,2) -1 )) 
-k_rqh(x::T,α) where {T<:AbstractVector} = zero(eltype(x)) 
-
-"""
-
-	mmdrq(x,y,α)
-
-	mmd with rq kernel
-
-"""
-mmdrq(x,y,α) = k_rqh(x,α) + k_rqh(y,α) - 2*k_rqh(x,y,α)
-mmdrq(x,y,α,n) = mmdrq(samplecolumns(x,n), samplecolumns(y,n), α)
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
-using IPMeasures
-using Test
-using IPMeasures: mapsum
+using IPMeasures, Test
+using IPMeasures: mapsum, pairwisel2, mmd, GaussianKernel
 
 @testset "mapsum" begin
 	d = randn(5,5)
@@ -8,3 +7,14 @@ using IPMeasures: mapsum
 	@test mapsum(exp, d , [1,5]) ≈ sum(exp.(d[[1,5],[1,5]]))
 	@test mapsum(exp, d , [1,2],[3,5]) ≈ sum(exp.(d[1:2,[3,5]]))
 end
+
+
+@testset "Gaussian MMD" begin
+	x = randn(3,10)
+	d = pairwisel2(x)
+	k = GaussianKernel(1.0)
+	i, j = [1,2,3], [4,5,6]
+	@test mmd(k, d, i, j) ≈ mmd(k, x[:,i],x[:,j])
+	@test mmd(k, randn(1,1000), randn(1,1000)) < 1e-2
+end
+
