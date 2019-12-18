f1b41c20273e53758ea1ab6e50a52c8ab0f29340
Niklas Heim <heim.niklas@gmail.com>
2019-12-17 23:33:16 -0800

    adopt updated interface of ConditionalDists

--- src/kl_divergence.jl
+++ src/kl_divergence.jl
@@ -17,7 +17,7 @@ end
 
 kl_divergence(p::Gaussian, q::Gaussian) = KLDivergence()(p, q)
 
-function (m::KLDivergence)(p::AbstractCGaussian{T}, q::Gaussian{T}, z::AbstractArray{T}) where T
+function (m::KLDivergence)(p::AbstractCGaussian, q::Gaussian, z::AbstractArray)
     (μ1, σ1) = mean_var(p, z)
     (μ2, σ2) = mean_var(q)
     _kld_gaussian(μ1, σ1, μ2, σ2)
