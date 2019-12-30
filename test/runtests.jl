24ecf66157212f9b1155cea8b3f4e31f99ed57aa
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2019-12-30 01:31:11 +0100

    Bump test coverage (#7)
    bump it

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,24 @@ using ConditionalDists
 using IPMeasures
 
 using IPMeasures: mapsum, mmd, mmdfromdist, mmd2_and_variance,
-    GaussianKernel, RQKernel, IMQKernel
+    GaussianKernel, RQKernel, IMQKernel, null_distribution, samplecolumns, split2
+
+@testset "samplecolumns" begin
+    xs = randn(2,5)
+    ys = samplecolumns(xs, 2)
+    @test sum(all(xs .≈ ys[:,1], dims=1)) == 1
+    @test sum(all(xs .≈ ys[:,2], dims=1)) == 1
+
+    ys = samplecolumns(xs, 12)
+    @test all(xs .== ys)
+end
+
+@testset "split2" begin
+    x = randn(2,10)
+    (a,b) = split2(x)
+    @test all(x[:,1:5] .== a)
+    @test all(x[:,6:10] .== b)
+end
 
 @testset "mapsum" begin
 	d = randn(5,5)
@@ -12,7 +29,6 @@ using IPMeasures: mapsum, mmd, mmdfromdist, mmd2_and_variance,
 	@test mapsum(exp, d , [1,5]) ≈ sum(exp.(d[[1,5],[1,5]]))
 	@test mapsum(exp, d , [1,2],[3,5]) ≈ sum(exp.(d[1:2,[3,5]]))
 end
-
 
 @testset "kernels" begin
 	x = randn(3,10)
@@ -22,6 +38,14 @@ end
 		@test mmdfromdist(k, d, i, j) .≈ mmd(k, x[:,i],x[:,j])
 		@test mmd(k, randn(1,1000), randn(1,1000)) < 1e-2
 	end
+end
+
+@testset "Null distribution" begin
+    k = GaussianKernel(1.0)
+    x = randn(100, 100)
+    n = 100
+    d = null_distribution(k,x,n)
+    @test size(d) == (100,)
 end
 
 include("mmd.jl")
